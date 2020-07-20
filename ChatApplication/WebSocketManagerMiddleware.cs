1e458e51b08612c40f17034d1e9ed9846472f6d7
muhammedshameem33 <shameempp33@gmail.com>
2020-07-20 07:50:26 +0530

    [muhammed shameem] git Add .

---
+++ ChatApplication/WebSocketManagerMiddleware.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChatApplication
+{
+    public class WebSocketManagerMiddleware
+    {
+        private readonly RequestDelegate next;
+        private WebSocketHandler webSocketHandler { get; set; }
+
+        public WebSocketManagerMiddleware(RequestDelegate next,
+                                            WebSocketHandler webSocketHandler)
+        {
+            this.next = next;
+            this.webSocketHandler = webSocketHandler;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (!context.WebSockets.IsWebSocketRequest)
+                return;
+
+            var socket = await context.WebSockets.AcceptWebSocketAsync();
+            await webSocketHandler.OnConnected(socket);
+
+            await Receive(socket, async (result, buffer) =>
+            {
+                if (result.MessageType == WebSocketMessageType.Text)
+                {
+                    await webSocketHandler.ReceiveAsync(socket, result, buffer);
+                    return;
+                }
+
+                else if (result.MessageType == WebSocketMessageType.Close)
+                {
+                    await webSocketHandler.OnDisconnected(socket);
+                    return;
+                }
+
+            });
+        }
+
+        private async Task Receive(WebSocket socket, Action<WebSocketReceiveResult, byte[]> handleMessage)
+        {
+            var buffer = new byte[1024 * 4];
+
+            while (socket.State == WebSocketState.Open)
+            {
+                var result = await socket.ReceiveAsync(buffer: new ArraySegment<byte>(buffer),
+                                                        cancellationToken: CancellationToken.None);
+
+                handleMessage(result, buffer);
+            }
+        }
+
+    }
+    public static class MiddleWareExtension
+    {
+        public static IApplicationBuilder MapWebSocketManager(this IApplicationBuilder app,
+                                                        PathString path,
+                                                        WebSocketHandler handler)
+        {
+            return app.Map(path, (_app) => _app.UseMiddleware<WebSocketManagerMiddleware>(handler));
+        }
+
+        public static IServiceCollection AddWebSocketManager(this IServiceCollection services)
+        {
+            services.AddTransient<ConnectionManager>();
+
+            foreach (var type in Assembly.GetEntryAssembly().ExportedTypes)
+            {
+                if (type.GetTypeInfo().BaseType == typeof(WebSocketHandler))
+                {
+                    services.AddSingleton(type);
+                }
+            }
+
+            return services;
+        }
+    }
+
+}
